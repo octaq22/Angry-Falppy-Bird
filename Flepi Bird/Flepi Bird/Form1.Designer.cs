3d9adf512f8fe6fdfab5f3d034769b178df428f2
octaq69 <74989387+octaq69@users.noreply.github.com>
2023-03-01 00:42:05 +0200

    Add files via upload

---
+++ Flepi Bird/Flepi Bird/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Flepi_Bird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.meniu_joc = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelrestart = new System.Windows.Forms.Label();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            this.meniu_joc.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 40;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 462);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Scor :";
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.ErrorImage = null;
+            this.p1.Image = ((System.Drawing.Image)(resources.GetObject("p1.Image")));
+            this.p1.InitialImage = null;
+            this.p1.Location = new System.Drawing.Point(174, 166);
+            this.p1.Margin = new System.Windows.Forms.Padding(2);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(92, 87);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1.TabIndex = 1;
+            this.p1.TabStop = false;
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.Transparent;
+            this.p3.Image = ((System.Drawing.Image)(resources.GetObject("p3.Image")));
+            this.p3.Location = new System.Drawing.Point(498, 330);
+            this.p3.Margin = new System.Windows.Forms.Padding(2);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(59, 136);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p3.TabIndex = 3;
+            this.p3.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.Image = ((System.Drawing.Image)(resources.GetObject("p2.Image")));
+            this.p2.Location = new System.Drawing.Point(563, 0);
+            this.p2.Margin = new System.Windows.Forms.Padding(2);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(55, 151);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2.TabIndex = 5;
+            this.p2.TabStop = false;
+            // 
+            // meniu_joc
+            // 
+            this.meniu_joc.BackColor = System.Drawing.Color.Yellow;
+            this.meniu_joc.Controls.Add(this.label3);
+            this.meniu_joc.Controls.Add(this.label2);
+            this.meniu_joc.Controls.Add(this.labelrestart);
+            this.meniu_joc.Location = new System.Drawing.Point(271, 188);
+            this.meniu_joc.Name = "meniu_joc";
+            this.meniu_joc.Size = new System.Drawing.Size(286, 166);
+            this.meniu_joc.TabIndex = 6;
+            this.meniu_joc.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Final de joc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Beige;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ieșire";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelrestart
+            // 
+            this.labelrestart.AutoSize = true;
+            this.labelrestart.BackColor = System.Drawing.Color.Beige;
+            this.labelrestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelrestart.Location = new System.Drawing.Point(91, 71);
+            this.labelrestart.Name = "labelrestart";
+            this.labelrestart.Size = new System.Drawing.Size(103, 31);
+            this.labelrestart.TabIndex = 0;
+            this.labelrestart.Text = "Restart";
+            this.labelrestart.Click += new System.EventHandler(this.labelrestart_Click);
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.Color.Transparent;
+            this.p4.Image = ((System.Drawing.Image)(resources.GetObject("p4.Image")));
+            this.p4.Location = new System.Drawing.Point(-2, 444);
+            this.p4.Margin = new System.Windows.Forms.Padding(2);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(753, 91);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p4.TabIndex = 4;
+            this.p4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(749, 500);
+            this.Controls.Add(this.meniu_joc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeydown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyup);
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            this.meniu_joc.ResumeLayout(false);
+            this.meniu_joc.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.GroupBox meniu_joc;
+        private System.Windows.Forms.Label labelrestart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox p4;
+    }
+}
+
---
+++ Flepi Bird/Flepi Bird/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Flepi_Bird
+{
+    public partial class Form1 : Form
+    {
+        int pipeSpeed = 8;
+        int gravity = 15;
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            meniu_joc.Hide();
+        }
+
+
+
+
+        private void endGame()
+        {
+
+            gameTimer.Stop();
+            p1.Image = Properties.Resources.E_editat;
+            p1.BackColor = Color.Transparent;
+            meniu_joc.Show();
+
+        }
+
+
+
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            p1.Top += gravity;
+            p3.Left -= pipeSpeed;
+            p2.Left -= pipeSpeed;
+            label1.Text = "Scor: " + score;
+
+
+
+            if (p3.Left < -50)
+            {
+
+                p3.Left = 400;
+                score++;
+            }
+            if (p2.Left < -136)
+            {
+
+                p2.Left = 450;
+                //score++;
+            }
+
+
+
+            if (p1.Bounds.IntersectsWith(p3.Bounds) ||
+                p1.Bounds.IntersectsWith(p2.Bounds) ||
+                p1.Bounds.IntersectsWith(p4.Bounds) || p1.Top < -25
+                )
+            {
+
+                endGame();
+
+            }
+
+
+
+            if (score > 5)
+            {
+                pipeSpeed = 15;
+            }
+
+        }
+
+        private void gamekeydown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+
+                gravity = -30;
+            }
+        }
+
+        private void gamekeyup(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Space)
+            {
+
+                gravity = 15;
+            }
+
+        }
+
+        private void labelrestart_Click(object sender, EventArgs e)
+        {
+            gameTimer.Start();
+            meniu_joc.Hide();
+            p1.Location = new Point(189, 160);
+            p2.Location = new Point(563, 0);
+            p3.Location = new Point(498, 330);
+            label1.Text = "Scor: 0";
+            score = 0;
+            p1.Image = Properties.Resources.A;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
+
+
